71962015c6e67740b3395c0a61c5f06b428c7b27
付小根16260 <xiaogen.fu@going-link.com>
2021-11-23 21:30:55 +0800

    <add>spring模板提交

---
+++ src/test/java/root/fu/spring/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package root.fu.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
